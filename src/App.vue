7beadc230fea16e6d6241c75bfc3fb1aaed05588
jelena mandzic <jelena.mandzic@gmail.com>
2020-11-12 20:17:24 +0100

    lllll

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <template>
   <v-app>
     <router-view></router-view>
+
     <!-- <about></about> -->
-    <!-- <home></home> -->
+    
   </v-app>
 </template>
 
 <script>
 // import About from "./views/About.vue";
-// import Home from "./views/Home.vue";
+ //import Home from "./views/Home.vue";
 
 export default {
   name: "App",
 
   components: {
     // About,
-    // Home
+     //Home
   },
 
   data: () => ({
